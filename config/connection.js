43f39441d394adc1f7c404644c1483c4d410be11
Arnaldo <arnaldosepulveda@gmail.com>
2024-02-20 22:21:51 -0400

    connect to postgresql

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,31 @@ const Sequelize = require('sequelize');
 
 // connection setup supports both local and JawsDB
 // (a MySQL add-on for Heroku) connections
-const sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  dialectOptions: {
-    decimalNumbers: true,
-  },
-});
+// const sequelize = process.env.JAWSDB_URL
+// ? new Sequelize(process.env.JAWSDB_URL)
+// : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     decimalNumbers: true,
+//   },
+// });
+
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 // configured Sequelize instance exported
 // making it available for use in app
